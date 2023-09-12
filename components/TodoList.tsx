1421c82a5047fd1fef0e4ecdd5d59e377b6858f8
bilalm354 <bilalm354@gmail.com>
2023-09-12 01:03:18 +0100

    made next test pass

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,9 +1,15 @@
 "use client"
 import { ChangeEvent, KeyboardEvent, useState } from 'react'
 
-const defaultTodos = [
-  { id: 1, text: 'Pay electric bill' },
-  { id: 2, text: 'Walk the dog' },
+interface Todo {
+  id: number,
+  text: string,
+  isChecked: boolean
+}
+
+const defaultTodos: Todo[] = [
+  { id: 1, text: 'Pay electric bill', isChecked: false },
+  { id: 2, text: 'Walk the dog', isChecked: false },
 ]
 
 export default function TodoList() {
@@ -18,21 +24,33 @@ export default function TodoList() {
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>): void {
     console.log(event.key)
     if (event.key === 'Enter') {
-      setTodos([...todos, { id: todos.length + 1, text: newTodo }]);
+      setTodos([...todos, { id: todos.length + 1, text: newTodo, isChecked: false }]);
       setNewTodo('');
     }
+  }
+
+  function onCheckBoxChange(event: ChangeEvent<HTMLInputElement>, todo: Todo): void {
+    setTodos(todos.map((t) => {
+      if (t.id === todo.id) {
+        return { ...t, isChecked: event.target.checked }
+      }
+      return t;
+    }))
   }
 
   return (
     <>
       <input type='text' placeholder='Add a new todo' onKeyDown={handleKeyDown} onChange={onNewTodoChange} className='text-black' data-test="new-todo" />
       <ul className='todo-list'>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input type='checkbox' />
-            <span>{todo.text}</span>
-          </li>
-        ))}
+        {todos.map((todo) => {
+          const { id, text, isChecked } = todo;
+          return (
+            <li key={id} className={isChecked ? 'completed' : ''}>
+              <input type='checkbox' onChange={(event) => onCheckBoxChange(event, todo)} checked={isChecked} />
+              <span>{text}</span>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -21,23 +21,17 @@ describe('example to-do app', () => {
       .should('have.text', newItem)
   })
 
-  it.skip('can check off an item as completed', () => {
-    // In addition to using the `get` command to get an element by selector,
-    // we can also use the `contains` command to get an element by its contents.
-    // However, this will yield the <label>, which is lowest-level element that contains the text.
-    // In order to check the item, we'll find the <input> element for this <label>
-    // by traversing up the dom to the parent element. From there, we can `find`
-    // the child checkbox <input> element and use the `check` command to check it.
+  // TODO: unskip below tests and make them pass
+  // TODO: add more tests as needed
+  // TODO: close related github issues
+  // TODO: create next github issues
+
+  it('can check off an item as completed', () => {
     cy.contains('Pay electric bill')
       .parent()
       .find('input[type=checkbox]')
       .check()
 
-    // Now that we've checked the button, we can go ahead and make sure
-    // that the list element is now marked as completed.
-    // Again we'll use `contains` to find the <label> element and then use the `parents` command
-    // to traverse multiple levels up the dom until we find the corresponding <li> element.
-    // Once we get that element, we can assert that it has the completed class.
     cy.contains('Pay electric bill')
       .parents('li')
       .should('have.class', 'completed')

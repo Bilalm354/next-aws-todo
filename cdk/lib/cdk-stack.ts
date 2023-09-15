be3d21d8cafe4b794776822ade7c6509f2fd95ac
bilalm354 <bilalm354@gmail.com>
2023-09-16 00:10:04 +0100

    cdk init

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.TableV2(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+    });
+  }
+}

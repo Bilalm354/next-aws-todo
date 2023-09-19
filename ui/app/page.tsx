735bdc5834c4b31a7a1e4847206c15878e530cd9
bilalm354 <bilalm354@gmail.com>
2023-09-19 22:37:33 +0100

    add react types to ./ui

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1 className='text-4xl font-bold'>Todo App</h1>
-      <TodoList /> 
+      <TodoList />
     </main>
   )
 }

01442565246b4f701defc04e4057189a1dda8f84
bilalm354 <bilalm354@gmail.com>
2023-09-20 00:45:22 +0100

    remove log

--- ui/app/api/route.ts
+++ ui/app/api/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
-  console.log('post function')
     const todo = await request.json()
 
     console.log({todo})
